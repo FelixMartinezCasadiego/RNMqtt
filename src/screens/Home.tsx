85b7cb28b808325d79e27744b64782d4ca1b9752
Felix Martinez Casadiego <felix.martinez@simonelectric.com>
2025-07-29 20:51:52 +0200

    feat: update Home.tsx

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,59 +1,47 @@
 import {Text, SafeAreaView, Button, StyleSheet} from 'react-native';
 import React, {useEffect, useState} from 'react';
-import Paho from 'paho-mqtt';
-import {SIMON_HOST, SIMON_PORT} from '@env';
+import * as Paho from 'paho-mqtt';
 
-//let client = new Paho.Client('broker.hivemq.com', 8000, `mqtt-async-test`);
-
-let client = new Paho.Client(
-  SIMON_HOST,
-  Number(SIMON_PORT),
+const client = new Paho.Client(
+  'broker.hivemq.com',
+  8000,
   `selba-app-${Math.random().toString(16).slice(3)}`,
 );
 
 const Home = () => {
   const [value, setValue] = useState(0);
+
   const onMessage = (message: Paho.Message) => {
-    if (message.destinationName === 'mqtt-async-test/value') {
+    if (message.destinationName === 'test/selba/value') {
       setValue(parseInt(message.payloadString));
     }
   };
-  const handleIncomingMessage = (messageArrived: Paho.Message) => {
-    onMessage(messageArrived);
-  };
+
   useEffect(() => {
     client.connect({
-      userName: '7935_r9jmeasfd68s0w4okgkc44ckkogo4g04ggwo8ooogk0w400o8',
-      password: '2eq2ptdp7kcg4ksss4k4488ww8sccgs0osgg84w4co4gk00gcs',
-      useSSL: true,
+      useSSL: false,
       onSuccess: () => {
-        console.log('Connected!');
-        client.subscribe('simon/things/1c9dc24bf4d0/1');
-        client.onMessageArrived = handleIncomingMessage;
-        //client.onMessageArrived = onMessage;
+        console.log('Connected to HiveMQ');
+        client.subscribe('test/selba/value');
+        client.onMessageArrived = onMessage;
       },
-      onFailure: value => {
-        console.log('onFailure ---> ', value);
+      onFailure: err => {
+        console.log('Connection failed:', err);
       },
     });
   }, []);
 
-  const changeValue = (client: Paho.Client) => {
-    const message: Paho.Message = new Paho.Message((value + 1).toString());
-    message.destinationName = 'mqtt-async-test/value';
+  const changeValue = () => {
+    const message = new Paho.Message((value + 1).toString());
+    message.destinationName = 'test/selba/value';
     client.send(message);
   };
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text>MQTT</Text>
+      <Text>MQTT HiveMQ</Text>
       <Text>Value is: {value}</Text>
-      <Button
-        onPress={() => {
-          changeValue(client);
-        }}
-        title="Press Me!"
-      />
+      <Button onPress={changeValue} title="Increase Value" />
     </SafeAreaView>
   );
 };
